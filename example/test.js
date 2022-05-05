068812c4ccbdad8fcde5aeff725ba25ceaf00a2b
rohit_verma <987@Open>
2022-05-05 10:54:56 +0530

    some test

--- example/test.js
+++ example/test.js
@@ -1,13 +1,20 @@
 import http from 'k6/http';
+import { check, sleep } from 'k6';
 
 export const options = {
   vus: 10,
-  duration: '10s',
+  stages: [
+    { duration: '30s', target: 20 },
+    { duration: '4m30s', target: 10 },
+    { duration: '20s', target: 0 },
+  ],
   thresholds: {
     'http_reqs{expected_response:true}': ['rate>10'],
   },
 };
 
 export default function () {
-  http.get('https://test.k6.io/');
+  const res = http.get('https://httpbin.test.k6.io/');
+  check(res, { 'status was 200': (r) => r.status == 200 });
+  sleep(1);
 }
